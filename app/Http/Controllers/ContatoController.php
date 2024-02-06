db23eb73aab1b83ecfde56c861d83dd3e23a564a
VictorLima96 <112532096+VictorLima96@users.noreply.github.com>
2024-02-06 08:09:02 -0300

    bruh

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
+use App\Models\Contato;
 
 class ContatoController extends Controller
 {
-    //
+    public function index(){
+        return view('index');
+    }
+
+   public function exibirFormContato(){
+        return view('cadastrar');
+   }
+
+   public function createContato(Request $request){
+    $contato = $request->validate(['nomeContato' => 'string|required',
+    'foneContato' => 'string|required',
+    'emailContato' => 'string|required']);
+
+    Contato::Create($contato);
+    return Redirect::route('index');
+
+   }
+
 }
